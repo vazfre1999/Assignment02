aec75edf2649b168b5ebc8db73eb1f74bf1e8966
vazfr <vazfr@DESKTOP-UU2EBGP>
2020-10-05 21:55:30 -0700

    Created methods using StringBuilder

--- CSE360/src/cse360assignment02/AddingMachine.java
+++ CSE360/src/cse360assignment02/AddingMachine.java
@@ -1,5 +1,32 @@
 package cse360assignment02;
 
 public class AddingMachine {
+  private int total;
+  StringBuilder string = new StringBuilder(0);
+  
+  public AddingMachine () {
+    total = 0;  // not needed - included for clarity
+  }
+  
+  public int getTotal () {
+    return 0;
+  }
+  
+  public void add (int value) {
+	  
+	  string.append(value).append( "+" );
+  }
 
+  public void subtract (int value) {
+	  string.append(value).append( "-" );
+  }
+
+  public String toString () {
+	  System.out.println(total + " " +string);
+	  return " ";
+  }
+
+  public void clear() {
+	  System.out.println(string.delete(0, string.length()));
+  }
 }
