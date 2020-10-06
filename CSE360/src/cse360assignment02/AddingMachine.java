d6c246ef1e5bcc316b82eef1154391afb5b79249
vazfr <vazfr@DESKTOP-UU2EBGP>
2020-10-05 22:00:44 -0700

    Added comments to file and edited spaces

--- CSE360/src/cse360assignment02/AddingMachine.java
+++ CSE360/src/cse360assignment02/AddingMachine.java
@@ -1,32 +1,38 @@
 package cse360assignment02;
 
 public class AddingMachine {
-  private int total;
-  StringBuilder string = new StringBuilder(0);
+	
+	//declaring variable and StringBuilder
+	private int total;
+	StringBuilder string = new StringBuilder(0);
   
-  public AddingMachine () {
-    total = 0;  // not needed - included for clarity
-  }
+	public AddingMachine () {
+		total = 0;  // not needed - included for clarity
+	}
   
-  public int getTotal () {
-    return 0;
-  }
+	public int getTotal () {	 
+		//returns total
+		return 0;
+	}
   
-  public void add (int value) {
-	  
-	  string.append(value).append( "+" );
-  }
+	public void add (int value) {
+		//appends value into StringBuilder
+		string.append(value).append( "+" );  
+	}
 
-  public void subtract (int value) {
-	  string.append(value).append( "-" );
-  }
+	public void subtract (int value) {
+		//appends value into StringBuilder
+		string.append(value).append( "-" );
+	}
 
-  public String toString () {
-	  System.out.println(total + " " +string);
-	  return " ";
-  }
+	public String toString () {
+		//print StringBuilder
+		System.out.println(total + " " +string);
+		return " ";
+	}
 
-  public void clear() {
-	  System.out.println(string.delete(0, string.length()));
-  }
-}
+	public void clear() {
+		//clear StringBuilder
+		System.out.println(string.delete(0, string.length()));
+	}
+}//end of AddingMachine Class
